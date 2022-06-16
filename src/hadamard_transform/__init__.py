e97f6ac7172d57142890544c1af513483dec62a7
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2022-06-16 15:02:05 +0300

    add batch and inplace versions

--- src/hadamard_transform/__init__.py
+++ src/hadamard_transform/__init__.py
@@ -1,13 +1,6 @@
-import math
-from functools import cache
+from math import sqrt
 
 import torch
-
-
-@cache
-def sqrt(n):
-    # cached version vectors' dimensions
-    return math.sqrt(n)
 
 
 def rademacher_like(x, prng):
@@ -15,8 +8,61 @@ def rademacher_like(x, prng):
     return torch.empty_like(x).bernoulli_(generator=prng) * 2 - 1
 
 
-def hadamard_transform(vec):
+def hadamard_transform(vec, batched=False):
     """fast Walsh–Hadamard transform
+
+    hadamard transform is not very numerically stable by nature (lots of subtractions)
+    should try and use with float64 when possible
+
+    :param vec: vec length is expected to be a power of 2! (or each row if it is batched)
+    :param batched: indicate whether to treat the first parameter as batch dimension
+    :return: the Hadamard transform of vec
+    """
+    original_shape = vec.shape
+    if not batched:
+        # add fake 1 batch dimension
+        # for making the code a follow a single (batched) path
+        vec = vec.unsqueeze(0)
+    batch_dim, vec_dim = vec.shape
+
+    h = 2
+    while h <= vec_dim:
+        hf = h // 2
+        vec = vec.view(batch_dim, vec_dim // h, h)
+
+        half_1, half_2 = vec[:, :, :hf], vec[:, :, hf:]
+
+        vec = torch.cat((half_1 + half_2, half_1 - half_2), dim=-1)
+
+        h *= 2
+
+    return (vec / sqrt(vec_dim)).view(*original_shape)
+
+
+def randomized_hadamard_transform(x, prng, batched=False):
+    if batched == 'same_rotation':
+        d = rademacher_like(x[0], prng)
+    else:
+        d = rademacher_like(x, prng)
+
+    return hadamard_transform(x * d, batched)
+
+
+def inverse_randomized_hadamard_transform(tx, prng, batched=False):
+    if batched == 'same_rotation':
+        d = rademacher_like(tx[0], prng)
+    else:
+        d = rademacher_like(tx, prng)
+
+    return hadamard_transform(tx, batched) * d
+
+
+def hadamard_transform_(vec):
+    """in-place fast Walsh–Hadamard transform
+
+    Generally, the in-place version is not recommended
+    since it is generally *not* faster.
+    It is appropriate when we otherwise run out of memory.
 
     hadamard transform is not very numerically stable by nature (lots of subtractions)
     should try and use with float64 when possible
@@ -25,43 +71,30 @@ def hadamard_transform(vec):
     :return: the Hadamard transform of vec
     """
     d = vec.numel()
-    original_shape = vec.shape
     h = 2
     while h <= d:
         hf = h // 2
         vec = vec.view(d // h, h)
 
-        # For an in=place version we could use
-        #  vec[:, :hf] += vec[:, hf:]
-        #  vec[:, hf:] *= -2
-        #  vec[:, hf:] += vec[:, :hf]
-        # Generally, the in-place version is not recommended
-        # since it is generally *not* faster, and the in-place
-        # side effect can easily introduce bugs.
-        # *It is appropriate when we otherwise run out of memory*.
-
-        # TODO add batch support with
-        # half_1, half_2 = batch[:, :, :hf], batch[:, :, hf:]
-        half_1, half_2 = vec[:, :hf], vec[:, hf:]
-
-        vec = torch.cat((half_1 + half_2, half_1 - half_2), dim=-1)
+        # this following is in place of
+        # half_1 = half_1 + half_2
+        # half_2 = (half_1 + half_2) - 2 * half_2 =  half_1 - half_2
+        vec[:, :hf] += vec[:, hf:]
+        vec[:, hf:] *= -2
+        vec[:, hf:] += vec[:, :hf]
 
         h *= 2
 
-    return (vec / sqrt(d)).view(*original_shape)
+    vec /= sqrt(d)
 
 
-def randomized_hadamard_transform(x, prng):
+def randomized_hadamard_transform_(x, prng):
     d = rademacher_like(x, prng)
+    x *= d
+    hadamard_transform_(x)
 
-    return hadamard_transform(x * d)
 
-
-def inverse_randomized_hadamard_transform(tx, prng):
+def inverse_randomized_hadamard_transform_(tx, prng):
     d = rademacher_like(tx, prng)
-
-    return hadamard_transform(tx) * d
-
-
-def example_function():
-    return 1 + 1
+    hadamard_transform_(tx)
+    tx *= d
--- tests/test_hadamard_transform.py
+++ tests/test_hadamard_transform.py
@@ -1,5 +1,75 @@
-from hadamard_transform import example_function
+import torch
+
+from hadamard_transform import hadamard_transform, randomized_hadamard_transform, inverse_randomized_hadamard_transform, \
+    hadamard_transform_, randomized_hadamard_transform_, inverse_randomized_hadamard_transform_
 
 
-def test_example_function():
-    assert example_function() == 2
+def test_hadamard_transform():
+    x = torch.rand(2 ** 10, dtype=torch.float64)
+    y = hadamard_transform(x)
+    assert torch.allclose(hadamard_transform(y), x)
+
+
+def test_batched_hadamard_transform():
+    x = torch.rand([10, 2 ** 10], dtype=torch.float64)
+    y = hadamard_transform(x, batched=True)
+    assert torch.allclose(hadamard_transform(y, batched=True), x)
+
+
+def test_randomized_hadamard_transform():
+    prng = torch.Generator(device='cpu')
+    x = torch.rand(2 ** 10, dtype=torch.float64)
+    seed = prng.seed()
+    assert torch.allclose(
+        inverse_randomized_hadamard_transform(
+            randomized_hadamard_transform(x, prng),
+            prng.manual_seed(seed)
+        ), x)
+
+
+def test_batched_randomized_hadamard_transform():
+    prng = torch.Generator(device='cpu')
+    x = torch.rand([10, 2 ** 10], dtype=torch.float64)
+    seed = prng.seed()
+    tx = randomized_hadamard_transform(x, prng, batched=True)
+    assert not torch.allclose(tx[1:], tx[0])
+    assert torch.allclose(
+        inverse_randomized_hadamard_transform(
+            tx,
+            prng.manual_seed(seed),
+            batched=True,
+        ), x)
+
+
+def test_batched_same_randomized_hadamard_transform():
+    prng = torch.Generator(device='cpu')
+    x = torch.rand([1, 2 ** 10], dtype=torch.float64).repeat(10, 1)
+    seed = prng.seed()
+    tx = randomized_hadamard_transform(x, prng, batched='same_rotation')
+    assert torch.allclose(tx[1:], tx[0])
+    assert torch.allclose(
+        inverse_randomized_hadamard_transform(
+            tx,
+            prng.manual_seed(seed),
+            batched='same_rotation',
+        ), x)
+
+
+def test_in_place_hadamard_transform():
+    x = torch.rand(2 ** 10, dtype=torch.float64)
+    original_x = torch.clone(x)
+    hadamard_transform_(x)
+    assert not torch.allclose(x, original_x)
+    hadamard_transform_(x)
+    assert torch.allclose(x, original_x)
+
+
+def test_in_place_randomized_hadamard_transform():
+    prng = torch.Generator(device='cpu')
+    x = torch.rand(2 ** 10, dtype=torch.float64)
+    original_x = torch.clone(x)
+    seed = prng.seed()
+    randomized_hadamard_transform_(x, prng)
+    assert not torch.allclose(x, original_x)
+    inverse_randomized_hadamard_transform_(x, prng.manual_seed(seed))
+    assert torch.allclose(x, original_x)
